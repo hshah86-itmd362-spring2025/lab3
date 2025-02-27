a33bb5138ca7d8cb16c4b57f5ddce2737f858a86
Hashir <70011762+HashMaster02@users.noreply.github.com>
2025-02-27 17:16:51 -0600

    added jquery logic to check the custom checkbox

--- js/site.js
+++ js/site.js
@@ -1,5 +1,11 @@
 // jQuery 3.x-style ready event and locally scoped $
-jQuery(function($) {
-  $('html').removeClass('nojs');
-  $('html').addClass('hasjs');
+jQuery(function ($) {
+  $("html").removeClass("nojs");
+  $("html").addClass("hasjs");
+  $("#alt-box").on("click", function () {
+    $("#age-verification").prop("checked", true);
+    $("#alt-box").html("<p>&#10004;</p>");
+    $("#alt-box").css("background-color", "#7e8287");
+    $("#alt-box").css("border-color", "#7e8287");
+  });
 });
